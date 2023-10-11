d0610761128908643a16a80d444785afd8a491ad
Henrique Pessoa <titepessoa@gmail.com>
2023-10-11 20:13:19 -0300

    finish hero, begin skills

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,45 +4,45 @@ module.exports = {
     es2022: true,
     browser: true,
   },
-  extends: ['eslint:recommended', 'plugin:astro/recommended', 'plugin:astro/jsx-a11y-strict'],
+  extends: ["eslint:recommended", "plugin:astro/recommended", "plugin:astro/jsx-a11y-strict"],
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
   rules: {},
   overrides: [
     {
-      files: ['*.js'],
+      files: ["*.js"],
       rules: {
-        'no-mixed-spaces-and-tabs': ['error', 'smart-tabs'],
+        "no-mixed-spaces-and-tabs": ["error", "smart-tabs"],
       },
     },
     {
-      files: ['*.astro'],
-      parser: 'astro-eslint-parser',
+      files: ["*.astro"],
+      parser: "astro-eslint-parser",
       parserOptions: {
-        parser: '@typescript-eslint/parser',
-        extraFileExtensions: ['.astro'],
+        parser: "@typescript-eslint/parser",
+        extraFileExtensions: [".astro"],
       },
       rules: {
-        'no-mixed-spaces-and-tabs': ['error', 'smart-tabs'],
+        "no-mixed-spaces-and-tabs": ["error", "smart-tabs"],
       },
     },
     {
-      files: ['*.ts'],
-      parser: '@typescript-eslint/parser',
-      extends: ['plugin:@typescript-eslint/recommended'],
+      files: ["*.ts"],
+      parser: "@typescript-eslint/parser",
+      extends: ["plugin:@typescript-eslint/recommended"],
       rules: {
-        '@typescript-eslint/no-unused-vars': [
-          'error',
-          { argsIgnorePattern: '^_', destructuredArrayIgnorePattern: '^_' },
+        "@typescript-eslint/no-unused-vars": [
+          "error",
+          { argsIgnorePattern: "^_", destructuredArrayIgnorePattern: "^_" },
         ],
-        '@typescript-eslint/no-non-null-assertion': 'off',
+        "@typescript-eslint/no-non-null-assertion": "off",
       },
     },
     {
-      files: ['**/*.astro/*.js', '*.astro/*.js'],
-      parser: '@typescript-eslint/parser',
+      files: ["**/*.astro/*.js", "*.astro/*.js"],
+      parser: "@typescript-eslint/parser",
     },
   ],
 };
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import tailwind from '@astrojs/tailwind';
-import compress from 'astro-compress';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import tailwind from "@astrojs/tailwind";
+import compress from "astro-compress";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
@@ -12,5 +13,12 @@ export default defineConfig({
       applyBaseStyles: false,
     }),
     compress(),
+    icon({
+      include: {
+        devicon: ["*"],
+        ion: ["*"],
+        mdi: ["*"],
+      },
+    }),
   ],
 });
